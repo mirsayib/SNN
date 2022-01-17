5c3a36df55c2a4ab9e9b6aeb8e4cae16ed4316bc
mirsayib <68058282+mirsayib@users.noreply.github.com>
2022-01-17 15:53:51 +0530

    more features

--- syblog/settings.py
+++ syblog/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-==-g(cqn6rgd^&#=&x=vxpd41$-1k_rkow+r%v^h72p_szw!z1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
