35fe385f5752babfc187e00c663cb916d8de1c98
mirsayib <68058282+mirsayib@users.noreply.github.com>
2022-01-30 22:17:48 +0530

    g

--- syblog/settings.py
+++ syblog/settings.py
@@ -2,23 +2,23 @@
 
 from pathlib import Path
 import os
+import dotenv
 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'random_default')
+SECRET_KEY = os.environ['SECRET_KEY']
+print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
