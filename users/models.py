bc93e5f6c98071a6a2686b71c07c0a71197e132c
mirsayib <68058282+mirsayib@users.noreply.github.com>
2022-02-02 20:00:04 +0530

    df

--- users/models.py
+++ users/models.py
@@ -9,15 +9,24 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        # Opening the uploaded image
+        im = Image.open(self.image)
 
-    #     img = Image.open(self.image)
-    #     if(img.width > 300 and img.height > 300):
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image)
+        output = BytesIO()
 
+        # Resize/modify the image
+        im = im.resize((300, 300))
+
+        # after modifications, save it to the output
+        im.save(output, format='JPEG', quality=90)
+        output.seek(0)
+
+        # change the imagefield value to be the newley modifed image value
+        self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
+                                        sys.getsizeof(output), None)
+
+        super(Profile, self).save(*args, **kwargs)
 
 
 
